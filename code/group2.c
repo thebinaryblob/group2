98883995b86b74c29eb8f130210394363402e445
The Binary Blob <balz.aschwanden@students.unibas.ch>
2017-10-23 14:12:50 +0200

    Add template files

---
+++ code/group2.c
@@ -0,0 +1,10 @@
+/* Group project IoT 2017 */
+
+#include "contiki.h"
+#include "net/rime.h"
+#include "dev/button-sensor.h"
+#include "dev/leds.h"
+#include "node-id.h"
+#include <stdio.h>
+
+#define MAX_RETRANSMISSIONS 4
