fcb2d2438fd2209c7da244ad4f9821cad5598818
David Bösiger <david.boesiger@bluewin.ch>
2017-12-05 00:13:15 +0100

    let the parser generate two charts one with x-Axis loop and the other with TARWIS-Time

--- tarwis_out/parser.py
+++ tarwis_out/parser.py
@@ -10,38 +10,55 @@ for file in glob.glob("*.wiseml"):
     root = xml.etree.ElementTree.parse(file).getroot()
     trace = root.find('{http://wisebed.eu/ns/wiseml/1.0}trace')
     data = {}
-    x = []
+    x1 = []
+    x2 = []
     y = []
     
     for child in trace:
         if child.tag == '{http://wisebed.eu/ns/wiseml/1.0}timestamp':
-            timestamp = Decimal(child.text)
+            timestamp = (float) (Decimal(child.text))
         elif child.tag == '{http://wisebed.eu/ns/wiseml/1.0}node':
             valueSplit = (child.find('{http://wisebed.eu/ns/wiseml/1.0}data').text).split(":")
             if valueSplit[0] == 'LoopTime':
     			loop_count = (float) (Decimal(valueSplit[1]))
     			node_time = valueSplit[2]
     			node_id = child.attrib['id']
-    			diff = (float) (Decimal(node_time) - timestamp)
+    			diff = ((float) (Decimal(node_time))) - timestamp
     			if not node_id in data:
     				data[node_id] = {}
     			data[node_id][loop_count] = diff
-    			x.append(loop_count)
+    			x1.append(loop_count)
+    			x2.append(timestamp)
     			y.append(diff)
     
-    print data
+    #print data
     
     fig = plt.figure()
     figure = fig.add_subplot(111)
-    figure.scatter(x,y)
-    figure.plot([0,200],[np.mean(y)] * 2)
-    figure.set_xlim([np.amin(x),np.amax(x)])
+    figure.scatter(x1,y)
+	
+    xmax = np.amax(x1);
+    xmin = np.amin(x1);
+	
+    figure.plot([0,xmax-xmin],[np.mean(y)] * 2)
+    figure.set_xlim([xmin,xmax])
     figure.set_ylim([np.amin(y),np.amax(y)])
-    
-    #fig = plt.figure()
-    fig.savefig('../images/'+file+'.png', dpi=72)
-    
-    #plt.show()
+    fig.savefig('../images/'+file+'_loopCount.png', dpi=72)
+    plt.close()
+	
+    fig = plt.figure()
+    figure = fig.add_subplot(111)
+    figure.scatter(x2,y)
+	
+    xmax = np.amax(x2);
+    xmin = np.amin(x2);
+	
+    figure.plot([0,xmax-xmin],[np.mean(y)] * 2)
+    figure.set_xlim([xmin,xmax])
+    figure.set_ylim([np.amin(y),np.amax(y)])
+    fig.savefig('../images/'+file+'_tarwisTime.png', dpi=72)
+    plt.close()
+  
 
 
 
