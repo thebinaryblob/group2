d1eedcbe2a11768a7241dcb0a258daaad7c96116
David Bösiger <david.boesiger@bluewin.ch>
2017-12-05 11:44:54 +0100

    added colors to be able to differentiate nodes

--- tarwis_out/parser.py
+++ tarwis_out/parser.py
@@ -10,10 +10,18 @@ for file in glob.glob("*.wiseml"):
     root = xml.etree.ElementTree.parse(file).getroot()
     trace = root.find('{http://wisebed.eu/ns/wiseml/1.0}trace')
     data = {}
+    nodes = []
+	# loop_count
     x1 = []
+	# tarwis time
     x2 = []
+	# time difference
     y = []
-    
+	#colors
+    cs = []
+	# 40 different colors
+    colors = plt.cm.rainbow(np.linspace(0,1,40))
+	
     for child in trace:
         if child.tag == '{http://wisebed.eu/ns/wiseml/1.0}timestamp':
             timestamp = (float) (Decimal(child.text))
@@ -24,18 +32,25 @@ for file in glob.glob("*.wiseml"):
     			node_time = valueSplit[2]
     			node_id = child.attrib['id']
     			diff = ((float) (Decimal(node_time))) - timestamp
+				
+				
     			if not node_id in data:
     				data[node_id] = {}
     			data[node_id][loop_count] = diff
+				
+    			if not node_id in nodes:
+    				nodes.append(node_id)
+				
     			x1.append(loop_count)
     			x2.append(timestamp)
     			y.append(diff)
+    			cs.append(colors[nodes.index(node_id)])
     
     #print data
     
     fig = plt.figure()
     figure = fig.add_subplot(111)
-    figure.scatter(x1,y)
+    figure.scatter(x1,y, color=cs)
 	
     xmax = np.amax(x1);
     xmin = np.amin(x1);
@@ -48,7 +63,7 @@ for file in glob.glob("*.wiseml"):
 	
     fig = plt.figure()
     figure = fig.add_subplot(111)
-    figure.scatter(x2,y)
+    figure.scatter(x2,y, color=cs)
 	
     xmax = np.amax(x2);
     xmin = np.amin(x2);
